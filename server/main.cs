a8304114be0991ac1522e0b208859b0489a7be72
Luya <aitou.lucas@gmail.com>
2024-10-05 15:20:33 +0200

    removed object example from LiteDB

--- server/main.cs
+++ server/main.cs
@@ -9,13 +9,6 @@ using System.Runtime.Intrinsics.X86;
 
 namespace Naninaim
 {
-    public class Customer
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string[] Phones { get; set; }
-        public bool IsActive { get; set; }
-    }
     class Program
     {
         
@@ -30,20 +23,10 @@ namespace Naninaim
 
             User user = new User("a nother username");
             Console.WriteLine(user.username + "'s waifu's name is " + user.waifu.name);  
-            // "Filename=/mnt/storage/storage/Projects/Nanina/save/database.db";
             
             using(var db = new LiteDatabase(@"/mnt/storage/storage/Projects/Nanina/save/database.db")){
-                Console.WriteLine("DB Loaded?");
+                Console.WriteLine("DB Loaded!");
                 
-                /*var customer = new Customer
-                { 
-                    Name = "John Doe", 
-                    Phones = new string[] { "8000-0000", "9000-0000" }, 
-                    IsActive = true
-                };
-                var col = db.GetCollection<Customer>("testing");
-                col.Insert(customer);
-                col.EnsureIndex(x => x.Name);*/
                 var user_col = db.GetCollection<PocoUser>("user2");
                 user_col.Insert(user.ToPoco());
                 user_col.EnsureIndex(x => x.userId);
