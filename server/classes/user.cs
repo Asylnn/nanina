2cafb21da6ec5016ec0bd546b5978904c448662d
Luya <aitou.lucas@gmail.com>
2025-02-04 14:54:47 +0100

    Adding default values for some parameters of User class

--- server/classes/user.cs
+++ server/classes/user.cs
@@ -38,14 +38,14 @@ public class User(string username, Ids ids)
     public Waifu waifu { get; set; } //OLD!! To delete soon when all users migrated
     public List<Waifu> waifus { get; set; } = [DBUtils.GetWaifu("0")];
     public string Id { get; set; } = CreateId();
-    public string theme { get; set; }
+    public string theme { get; set; } = "dark_theme";
     public Ids ids { get; set; } = ids;
-    public Tokens tokens { get; set; }
+    public Tokens tokens { get; set; } = new ();
     public string locale { get; set; } = "us-en";
-    public string avatarPATH { get; set; }
+    public string avatarPATH { get; set; } = "";
     public StatCount statCount { get; set; } = new();
-    public List<Fight> fights { get; set; }
-    public int gacha_currency { get; set; }
+    public List<Fight> fights { get; set; } = [];
+    public int gacha_currency { get; set; } = 0;
     public Dictionary<string, PullBannerHistory> pullBannerHistory { get; set; }
     public Verification verification { get; set; } = new() { osuVerificationCode=null, osuVerificationCodetimestamp = 0, isOsuIdVerified=false };
     public long claimTimestamp { get; set; }
