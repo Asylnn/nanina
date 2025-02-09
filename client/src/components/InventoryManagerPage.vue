56f64b62ce062bf17d5613b8e76763ef4a2c91c1
Luya <aitou.lucas@gmail.com>
2025-02-09 21:35:28 +0100

    Add Inventory Manager Page (mistake from merging)

---
+++ client/src/components/InventoryManagerPage.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import type Equipment from '@/classes/inventory/equipment';
+import type Material from '@/classes/inventory/material';
+import type UserConsumable from '@/classes/inventory/user_consumable';
+import type WaifuConsumable from '@/classes/inventory/waifu_consumable';
+
+//Selectionner un user pour regarder son inventaire et pouvoir le modifier ##PLUS TARD
+
+//
+
+
+
+export default {
+    name : "InventoryManagerPage",
+    data() {
+        return {
+            page: 1,
+            equipment: new Array<Equipment>(),
+            user_consumable: new Array<UserConsumable>(),
+            waifu_consumable: new Array<WaifuConsumable>(),
+            material: new Array<Material>(),
+        }
+    },
+}
+
+
+</script>
+<template>
+    Bienvenue a l'inventaire manager
+</template>
+
+<style lang="css" scoped>
+
+
+</style>
