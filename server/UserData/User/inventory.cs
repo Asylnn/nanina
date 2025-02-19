3d44c16fe337af765bb65e8044962894e1e72124
Luya <aitou.lucas@gmail.com>
2025-02-19 16:13:31 +0100

    Fix missing get set in inventory

--- server/UserData/User/inventory.cs
+++ server/UserData/User/inventory.cs
@@ -4,7 +4,7 @@ namespace Nanina.UserData
 {
     public class Inventory 
     {
-        public uint inventoryIdCounter = 0;
+        public uint inventoryIdCounter {get; set;} = 0;
         public List<Equipment> equipment {get; set;} = [];
         public List<Item> material {get; set;} = [];
         public List<Item> userConsumable {get; set;} = [];
@@ -12,8 +12,10 @@ namespace Nanina.UserData
 
         public void AddEquipment(Equipment obtainedEquipment) 
         {
+            
             obtainedEquipment.inventoryId = inventoryIdCounter;
             inventoryIdCounter++;
+            //Console.WriteLine(inventoryIdCounter);
             equipment.Add(obtainedEquipment);
         }
         public void AddMaterial(Item obtainedMaterial)
