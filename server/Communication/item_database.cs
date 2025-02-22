78c1d6d366b905c49a0061bb43d3c14a04131058
Glou-Glou <salutjesuisunpoubeau@gmail.com>
2025-02-22 18:26:37 +0100

    separate Equipment from Item

--- server/Communication/item_database.cs
+++ server/Communication/item_database.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using LiteDB;
 using Nanina.Database;
 using Nanina.UserData.ItemData;
@@ -24,19 +25,50 @@ namespace Nanina.Communication
         */
         protected void ProvideItemDatabase(string userId)
         {
-            var user = DBUtils.Get<User>(x => x.Id == userId);
+            var user = DBUtils.Get<UserData.User>(x => x.Id == userId);
             if(user == null) 
                 {Send(ClientNotification.NotificationData("User", "You can't perform this action without being connected!", 1)); return ;}
             if(!user.admin)
                 {Send(ClientNotification.NotificationData("admin", "You don't have the permissions for this action!", 0)); return;}
 
             /* Get item col to find all and send a websocket containing the item database
-            
             */
             Console.WriteLine("je rentre dans send db client item");
             DBUtils.SendDatabaseToClient<Item>(ID);
         }
+
+        protected void ProvideEquipmentDatabase(string userId)
+        {
+            var user = DBUtils.Get<UserData.User>(x => x.Id == userId);
+            if(user == null) 
+                {Send(ClientNotification.NotificationData("User", "You can't perform this action without being connected!", 1)); return ;}
+            if(!user.admin)
+                {Send(ClientNotification.NotificationData("admin", "You don't have the permissions for this action!", 0)); return;}
+
+            /* Get item col to find all and send a websocket containing the item database
+            */
+            Console.WriteLine("je rentre dans send db client equipment");
+            DBUtils.SendDatabaseToClient<Equipment>(ID);
+        }
         protected void UpdateItemDatabase(ClientWebSocketResponse rawData)
+        {
+            var user = DBUtils.Get<UserData.User>(x => x.Id == rawData.userId);
+
+            if(user == null)
+                {Send(ClientNotification.NotificationData("User", "You can't perform this action without being connected!", 1)); return ;}
+            if(user.admin == false)
+                {Send(ClientNotification.NotificationData("admin", "You don't have the permissions for this action!", 0)); return;}
+            if(!Global.config.dev) 
+                {Send(ClientNotification.NotificationData("admin", "The server isn't in developpement mode, you can't do this action", 0)); return;}
+
+            var itemDBResponse = JsonConvert.DeserializeObject<ItemDBResponse>(rawData.data);
+
+            IEnumerable<Item> items = itemDBResponse.user_consumable.Concat(itemDBResponse.material).Concat(itemDBResponse.waifu_consumable);
+            DBUtils.Rebuild( items.ToArray());
+            Send(ClientNotification.NotificationData("admin", "updated the item database!", 0));
+        }
+
+        protected void UpdateEquipmentDatabase(ClientWebSocketResponse rawData)
         {
 
             var user = DBUtils.Get<UserData.User>(x => x.Id == rawData.userId);
@@ -48,29 +80,9 @@ namespace Nanina.Communication
             if(!Global.config.dev) 
                 {Send(ClientNotification.NotificationData("admin", "The server isn't in developpement mode, you can't do this action", 0)); return;}
 
-            var items = JsonConvert.DeserializeObject<ItemDBResponse>(rawData.data);
-            
-            /* Get item col to delete all all then add all back*/
-            var itemCol = DBUtils.GetCollection<Item>();
-
-
-            /* Get equip col to help insert equipments back in the db*/
-            var equipmentCol = DBUtils.GetCollection<Equipment>();
-            itemCol.DeleteAll();
-            foreach (var item in items.material) {
-                InsertItem(item, itemCol);
-            }
-            foreach (var item in items.waifu_consumable) {
-                InsertItem(item, itemCol);
-            }
-            foreach (var item in items.user_consumable) {
-                InsertItem(item, itemCol);
-            }
-            foreach (var item in items.equipment) {
-                InsertEquipment(item, itemCol, equipmentCol);
-            }
-            Send(ClientNotification.NotificationData("admin", "updated the item database!", 0));
-            
+            var itemDBResponse = JsonConvert.DeserializeObject<ItemDBResponse>(rawData.data);
+            DBUtils.Rebuild( itemDBResponse.equipment.ToArray());
+            Send(ClientNotification.NotificationData("admin", "updated the equipment database!", 0));
         }
 
         protected void ProvideSetDatabase(string userId)
@@ -101,16 +113,6 @@ namespace Nanina.Communication
             
             Send(ClientNotification.NotificationData("admin", "updated the set database!", 0));
             
-        }
-
-        public static void InsertEquipment(Equipment item, ILiteCollection<Item> itemCol, ILiteCollection<Equipment> col){
-            InsertItem(item, itemCol);
-            col.EnsureIndex(x => x.setId, false);
-        }
-        public static void InsertItem(Item item, ILiteCollection<Item> col){
-            col.Insert(item);
-            col.EnsureIndex(x => x.id, true);
-            col.EnsureIndex(x => x.type, false);
         }
     }
 }
