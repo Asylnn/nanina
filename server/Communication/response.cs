81b392cab297e3f799f214f3390bb91c6c9394b1
Luya <aitou.lucas@gmail.com>
2025-02-26 05:08:02 +0100

    Add try catch for onMessage

--- server/Communication/response.cs
+++ server/Communication/response.cs
@@ -11,101 +11,111 @@ namespace Nanina.Communication
         //static private List<WS> webSocketConnections = [];
         protected override void OnMessage(MessageEventArgs e)
         {
-            Console.WriteLine("DATA : " + (string) e.Data); 
-            ClientWebSocketResponse rawData = Newtonsoft.Json.JsonConvert.DeserializeObject<ClientWebSocketResponse>(e.Data);
-            using var db = new LiteDatabase($@"{Global.config.database_path}");
-            switch (rawData.type) {
+            try
+            {
+                Console.WriteLine("DATA : " + (string) e.Data); 
+                ClientWebSocketResponse rawData = Newtonsoft.Json.JsonConvert.DeserializeObject<ClientWebSocketResponse>(e.Data);
+                using var db = new LiteDatabase($@"{Global.config.database_path}");
+                switch (rawData.type) {
 
-                //Recieve a request from the client to get a user from a sessionId stored in cookies.
-                //It check the database for that sessionId and return, if found, the user associated with that session ID.
-                case "update theme": 
-                    UpdateTheme(rawData);
-                    break;
-                
-                case "update osu id":  
-                    UpdateOsuId(rawData);
-                    break;
-
-                /*case "request waifu db" :
-                    ProvideWaifuDatabase(rawData);
-                    break;
-                case "request item db" :
-                    ProvideItemDatabase(rawData);
-                    break;
-                case "request set db":
-                    ProvideSetDatabase(rawData);
-                    break;*/
-                case "change locale":
-                    DBUtils.Get<Session>(session => session.id == rawData.sessionId).UpdateLocale(rawData.data);
-                    break;
-                case "update prefered game":
-                    UpdatePreferedGame(rawData);
-                    break;
-                case "update waifu db": 
-                    UpdateWaifuDatabase(rawData);
-                    break;
-                case "update item db": 
-                    UpdateItemDatabase(rawData);
-                    UpdateEquipmentDatabase(rawData);
-                    break;
-                case "update set db":
-                    UpdateSetDatabase(rawData);
-                    break;
-                case "get map to fight": 
-                    StartFight(rawData);
-                    break;
-
-                 case "claim fight":
-                    ClaimFight(rawData);
-                    break;
-
-                case "get session id":
-                    ProvideSessionToClient(rawData);
-                    break;
+                    //Recieve a request from the client to get a user from a sessionId stored in cookies.
+                    //It check the database for that sessionId and return, if found, the user associated with that session ID.
+                    case "update theme": 
+                        UpdateTheme(rawData);
+                        break;
                     
-                case "add beatmap with id": 
-                    AddMapToDatabase(rawData);
-                    break;
+                    case "update osu id":  
+                        UpdateOsuId(rawData);
+                        break;
 
-                case "connect with discord":
-                    DiscordLogin(rawData);
-                    break;
-                case "pull request":
-                    Pull(rawData);
-                    break;
-                case "verify osu id":
-                    VerifyOsuId(rawData);
-                    break;
-                case "get map back":
-                    SendMapToClient(rawData);
-                    break;
-                case "start dungeon":
-                    StartDungeon(rawData);
-                    break;
-                case "stop dungeon":
-                    StopDungeon(rawData);
-                    break;
-                case "unequip item":
-                    UnequipItem(rawData);
-                    break;
-                
-                case "disconect":
-                    Disconnect(rawData);
-                    break;
-                case "equip item":
-                    EquipItem(rawData);
-                    break;
-                case "verify maimai token":
-                    VerifyMaimaiToken(rawData);
-                    break;
+                    /*case "request waifu db" :
+                        ProvideWaifuDatabase(rawData);
+                        break;
+                    case "request item db" :
+                        ProvideItemDatabase(rawData);
+                        break;
+                    case "request set db":
+                        ProvideSetDatabase(rawData);
+                        break;*/
+                    case "change locale":
+                        DBUtils.Get<Session>(session => session.id == rawData.sessionId).UpdateLocale(rawData.data);
+                        break;
+                    case "update prefered game":
+                        UpdatePreferedGame(rawData);
+                        break;
+                    case "update waifu db": 
+                        UpdateWaifuDatabase(rawData);
+                        break;
+                    case "update item db": 
+                        UpdateItemDatabase(rawData);
+                        UpdateEquipmentDatabase(rawData);
+                        break;
+                    case "update set db":
+                        UpdateSetDatabase(rawData);
+                        break;
+                    case "get map to fight": 
+                        StartFight(rawData);
+                        break;
 
-                case "get possible user level up loot":
-                    GetLevelRewards(rawData);
-                    break;
-                case "get level reward":
-                    GetReward(rawData);
-                    break;
+                    case "claim fight":
+                        ClaimFight(rawData);
+                        break;
+
+                    case "get session id":
+                        ProvideSessionToClient(rawData);
+                        break;
+                        
+                    case "add beatmap with id": 
+                        AddMapToDatabase(rawData);
+                        break;
+
+                    case "connect with discord":
+                        DiscordLogin(rawData);
+                        break;
+                    case "pull request":
+                        Pull(rawData);
+                        break;
+                    case "verify osu id":
+                        VerifyOsuId(rawData);
+                        break;
+                    case "get map back":
+                        SendMapToClient(rawData);
+                        break;
+                    case "start dungeon":
+                        StartDungeon(rawData);
+                        break;
+                    case "stop dungeon":
+                        StopDungeon(rawData);
+                        break;
+                    case "unequip item":
+                        UnequipItem(rawData);
+                        break;
+                    
+                    case "disconect":
+                        Disconnect(rawData);
+                        break;
+                    case "equip item":
+                        EquipItem(rawData);
+                        break;
+                    case "verify maimai token":
+                        VerifyMaimaiToken(rawData);
+                        break;
+
+                    case "get possible user level up loot":
+                        GetLevelRewards(rawData);
+                        break;
+                    case "get level reward":
+                        GetReward(rawData);
+                        break;
+                }
             }
+            catch(Exception err)
+            {
+                Console.Error.WriteLine(err.Message);
+                using var stream = File.AppendText("../errors");
+                stream.WriteLine(err.Message);
+            }
+            
         }
         protected override void OnClose(CloseEventArgs e)
         {
