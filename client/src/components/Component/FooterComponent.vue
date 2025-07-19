d5bb6c9d4b36d9599d2cce8d01cd77f6d8f7fdf2
luya <aitou.lucas@gmail.com>
2025-07-19 12:31:10 +0000

    add class clickable to all the links in the footer

--- client/src/components/Component/FooterComponent.vue
+++ client/src/components/Component/FooterComponent.vue
@@ -25,19 +25,19 @@ export default {
     <footer>
         <div class="category">
             <div class="categoryName">Contact Us</div><br>
-            <a target="_blank" href="https://github.com/Asylnn/nanina">Github</a><br>
-            <a target="_blank" href="https://discord.gg/xPBGjx2UXG" >Discord</a><br>
-            <a target="_blank" href="mailto:nanina.buisness@proton.me">EMail</a><br>
+            <a class="clickable"  target="_blank" href="https://github.com/Asylnn/nanina">Github</a><br>
+            <a class="clickable"  target="_blank" href="https://discord.gg/xPBGjx2UXG" >Discord</a><br>
+            <a class="clickable"  target="_blank" href="mailto:nanina.buisness@proton.me">EMail</a><br>
         </div>
         <div class="category">
             <div class="categoryName">Legal Stuff</div><br>
-            <span @click="$emit('page-change', 18)">Privacy</span><br>
-            <span>Terms</span><br>
+            <span class="clickable" @click="$emit('page-change', 18)">Privacy</span><br>
+            <span class="clickable">Terms</span><br>
         </div>
         <div class="category">
             <div class="categoryName">About Us</div><br>
-            <a>Who we are</a><br>
-            <a>Wiki</a>
+            <a class="clickable" >Who we are</a><br>
+            <a class="clickable" >Wiki</a>
         </div>
     </footer>
     
