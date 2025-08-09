ee9c93f4bbaf918736d739cf6d56f6cd8da3b481
luya <aitou.lucas@gmail.com>
2025-08-09 15:18:13 +0000

    Add forgotten required keyword to class osu user

--- server/Osu/user.cs
+++ server/Osu/user.cs
@@ -2,17 +2,17 @@ namespace Nanina.Osu
 {
     public class User 
     {
-        public string avatar_url;
-        public string country_code;
-        public string default_group;
+        public required string avatar_url;
+        public required string country_code;
+        public required string default_group;
         public double id;
         public bool is_active;
         public bool is_bot;
         public bool is_deleted;
         public bool is_supporter;
-        public string last_visit;
+        public required string last_visit;
         public bool pm_friends_only;
-        public string profile_colour;
-        public string username;
+        public required string profile_colour;
+        public required string username;
     }
 }
