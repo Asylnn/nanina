f487437ddee10fe0d3c0390d4b3c221c06884c41
Luya <aitou.lucas@gmail.com>
2025-10-05 12:13:21 +0200

    Add forgotten getters and setters

--- server/UserData/Log/dungeon_log.cs
+++ server/UserData/Log/dungeon_log.cs
@@ -2,7 +2,7 @@ namespace Nanina.UserData.Log;
 
 public class DungeonLog(short[] waifuLVL, short floor, long clearTime, double[] claimDmg)
 {
-    public short floor = floor;
+    public short floor { get; set; } = floor;
     public short[] waifuLVL { get; set; } = waifuLVL;
     public long timestamp { get; set; } = Utils.GetTimestamp();
     public long clearTime { get; set; } = clearTime;
--- server/UserData/Log/energy_log.cs
+++ server/UserData/Log/energy_log.cs
@@ -2,10 +2,10 @@ namespace Nanina.UserData.Log;
 
 public class EnergyLog(double energy, double max_energy, double energy_spent, double gacha_currency)
 {
-    public double energy = energy;
-    public double max_energy = max_energy;
-    public double energy_spent = energy_spent;
-    public double gacha_currency = gacha_currency;
+    public double energy { get; set; } = energy;
+    public double max_energy { get; set; } = max_energy;
+    public double energy_spent { get; set; } = energy_spent;
+    public double gacha_currency { get; set; } = gacha_currency;
     public long timestamp { get; set; } = Utils.GetTimestamp();
 
 }
